6b97172eadb3e7957b00df8cfdf8ba76c4b50efc
Nikita Raj <nikitaraj3804@gmail.com>
2026-08-22 09:57:35 +0530

    Update users.py

--- backend/models/users.py
+++ backend/models/users.py
@@ -1,11 +1,1 @@
-from sqlalchemy import Column, Integer, String
-from backend.database.database import Base
 
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    employee_id = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-    role = Column(String, default="employee")  # "employee" or "hr"
