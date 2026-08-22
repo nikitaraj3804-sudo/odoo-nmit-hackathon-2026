fe6b984b927bece8d21fb6470682013e2e3a7ecf
Nikita Raj <nikitaraj3804@gmail.com>
2026-08-22 09:57:21 +0530

    Update payroll.py

--- backend/models/payroll.py
+++ backend/models/payroll.py
@@ -1,12 +1,1 @@
-from sqlalchemy import Column, Integer, Float, ForeignKey
-from backend.database.database import Base
 
-class Payroll(Base):
-    __tablename__ = "payroll"
-
-    id = Column(Integer, primary_key=True, index=True)
-    employee_id = Column(Integer, ForeignKey("employees.id"))
-    basic_salary = Column(Float)
-    hra = Column(Float, default=0.0)
-    deductions = Column(Float, default=0.0)
-    net_salary = Column(Float)
